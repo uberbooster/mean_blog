7c3fb9f1c4d5c98d0af84b43a1eb66324af4fe60
UberBooster <info@uberbooster.com>
2016-08-08 21:02:46 -0400

    set up user service for get and put

--- public/js/controllers/SimpleController.js
+++ public/js/controllers/SimpleController.js
@@ -2,9 +2,25 @@
   angular.module('ngBlog')
         .controller('SimpleController', SimpleControllerF);
 
-  SimpleControllerF.$inject = ['$scope'];
+  SimpleControllerF.$inject = ['$scope', 'UserService'];
 
-  function SimpleControllerF($scope){
-    $scope.message = 'Hey! Angular works!'
+  function SimpleControllerF($scope, UserService){
+    $scope.message = 'Hey! Angular works!';
+
+    UserService.getAllUsers()
+              .then(function(response){
+                console.log(response);
+              });
+    var userObj = {
+      firstName: 'Lil',
+      lastName: 'Jon',
+      age: 41,
+      email: 'lilfunnyman@jon.com'
+    };
+    UserService.createUser(userObj)
+              .then(function(response){
+                console.log(response);
+              });
+    
   };
 })();
--- public/js/services/UserService.js
+++ public/js/services/UserService.js
@@ -5,6 +5,31 @@
   UserServiceF.$inject = ['$http'];
 
   function UserServiceF($http){
+    var baseUrl = 'https://calm-atoll-49293.herokuapp.com/users/';
+    var service = {
+      getAllUsers: getAllUsersF,
+      createUser: createUserF,
+      getOneUser: getOneUserF,
+      updateUser: updateUserF,
+      deleteUser: deleteUserF
+    };
+    return service;
 
-  }
-})();
+    function getAllUsersF(){
+      return $http.get(baseUrl);
+    };
+    function createUserF(userObj){
+      return $http.post(baseUrl, userObj);
+    };
+    function getOneUserF(userId){
+
+    };
+    function updateUserF(userId, updateInfo){
+
+    };
+    function deleteUserF(userId){
+
+    };
+
+  };
+}());
